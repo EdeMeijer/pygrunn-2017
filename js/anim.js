b74b8362a0b3c99a9e5548fc1613f5f508c77a2c
Ede Meijer <ede.meijer88@gmail.com>
2017-05-14 21:44:26 +0200

    Gradient descent slide

--- js/anim.js
+++ js/anim.js
@@ -2,7 +2,6 @@
 
 (function () {
     const SAMPLE_RADIUS = 7;
-    const MARGIN_X = 250;
     const HEIGHT = 300;
     const WIDTH = 500;
 
@@ -10,9 +9,9 @@
     const COL_BLUE = '#444488';
     const COL_GREEN = '#3c9f3d';
 
-    bindLinearRegression('linear-regression-slide', 'linear-regression-diagram');
+    bindLinearRegression('linear-regression-slide', 'linear-regression-diagram', 250);
 
-    bindLogisticRegression('logistic-regression-slide', 'logistic-regression-diagram');
+    bindLogisticRegression('logistic-regression-slide', 'logistic-regression-diagram', 250);
 
     bindDecisionTrees('decision-trees-slide', 'decision-trees-diagram');
 
@@ -35,6 +34,8 @@
             animDuration: 400
         }
     );
+
+    bindGradientDescent('gd-fragment', 'gd-diagram');
 
     function rnd(seed) {
         let m_w = typeof seed === 'undefined' ? 123456789 : seed;
@@ -60,7 +61,7 @@
         return rad * 180 / Math.PI;
     }
 
-    function lineOptions(x1, y1, x2, y2) {
+    function lineUpdateOptions(x1, y1, x2, y2) {
         const x = (x1 + x2) / 2;
         const y = (y1 + y2) / 2;
 
@@ -68,14 +69,20 @@
         const length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 
         return {
-            left: 0,
-            top: 0,
             x: x,
             y: y,
-            shape: 'line',
             radius: length / 2,
             angle: rad2deg(angle)
         };
+    }
+
+    function lineOptions(x1, y1, x2, y2) {
+        const opt = lineUpdateOptions(x1, y1, x2, y2);
+        opt.left = 0;
+        opt.top = 0;
+        opt.shape = 'line';
+        opt.strokeLinecap = 'round';
+        return opt;
     }
 
     function addGridLine(parent, x1, y1, x2, y2) {
@@ -100,12 +107,8 @@
         })
     }
 
-    function predictLineOptions(intercept, slope) {
-        return lineOptions(MARGIN_X, HEIGHT - intercept, MARGIN_X + WIDTH, HEIGHT - (intercept + WIDTH * slope));
-    }
-
-    function addPredictionLine(parent, intercepts, slopes) {
-        const opt = predictLineOptions(intercepts[0], slopes[0]);
+    function addPredictionLine(parent, intercepts, slopes, marginX) {
+        const opt = lineOptions(marginX, HEIGHT - intercepts[0], marginX + WIDTH, HEIGHT - (intercepts[0] + WIDTH * slopes[0]));
         opt.parent = parent;
         opt.strokeWidth = 5;
         opt.isShowStart = true;
@@ -115,20 +118,39 @@
         let shape = new mojs.Shape(opt);
 
         for (let i = 1; i < intercepts.length; i++) {
-            const opt2 = predictLineOptions(intercepts[i], slopes[i]);
-            opt2.duration = 2000;
-            shape = shape.then(opt2);
+            shape = updatePredictLine(shape, intercepts[i], slopes[i], null, marginX)
         }
 
         return shape;
     }
 
-    function bindLinearRegression(fragmentOrSlideId, elementId) {
+    function addStepLine(parent, x1, y1, x2, y2, marginX, delay) {
+        const opt = lineOptions(marginX + x1, HEIGHT - y1, marginX + x1 + (x2 - x1) * 0.001, HEIGHT - (y1 + (y2 - y1) * 0.001));
+        opt.parent = parent;
+        opt.strokeWidth = 5;
+        opt.isShowStart = false;
+        opt.stroke = '#FFF';
+        opt.duration = delay;
+        opt.strokeOpacity = 0;
+
+        const opt2 = lineUpdateOptions(marginX + x1, HEIGHT - y1, marginX + x2, HEIGHT - y2);
+        opt2.duration = 250;
+
+        return new mojs.Shape(opt).then({strokeOpacity: 1.0, duration: 10}).then(opt2);
+    }
+
+    function updatePredictLine(shape, intercept, slope, duration, marginX) {
+        const opt2 = lineUpdateOptions(marginX, HEIGHT - intercept, marginX + WIDTH, HEIGHT - (intercept + WIDTH * slope));
+        opt2.duration = duration || 2000;
+        return shape.then(opt2);
+    }
+
+    function bindLinearRegression(fragmentOrSlideId, elementId, marginX) {
         const $fragmentOrSlide = $('#' + fragmentOrSlideId);
         const parent = document.getElementById(elementId);
 
-        addGridLine(parent, MARGIN_X, 0, MARGIN_X, HEIGHT);
-        addGridLine(parent, MARGIN_X, HEIGHT, MARGIN_X + WIDTH, HEIGHT);
+        addGridLine(parent, marginX, 0, marginX, HEIGHT);
+        addGridLine(parent, marginX, HEIGHT, marginX + WIDTH, HEIGHT);
 
         const random = rnd(12345);
 
@@ -137,13 +159,14 @@
 
         for (let x = 30; x < WIDTH - 15; x += random() * 30) {
             const y = intercept + x * slope + randomNormal(random) * (5 + x * 0.1);
-            addSample(parent, MARGIN_X + x, HEIGHT - y, COL_ORANGE);
+            addSample(parent, marginX + x, HEIGHT - y, COL_ORANGE);
         }
 
         const predictLine = addPredictionLine(
             parent,
             [0, intercept],
-            [0.1, slope]
+            [0.1, slope],
+            marginX
         );
 
         $fragmentOrSlide.on('show', () => {
@@ -155,12 +178,12 @@
         });
     }
 
-    function bindLogisticRegression(fragmentOrSlideId, elementId) {
+    function bindLogisticRegression(fragmentOrSlideId, elementId, marginX) {
         const $fragmentOrSlide = $('#' + fragmentOrSlideId);
         const parent = document.getElementById(elementId);
 
-        addGridLine(parent, MARGIN_X, 0, MARGIN_X, HEIGHT);
-        addGridLine(parent, MARGIN_X, HEIGHT, MARGIN_X + WIDTH, HEIGHT);
+        addGridLine(parent, marginX, 0, marginX, HEIGHT);
+        addGridLine(parent, marginX, HEIGHT, marginX + WIDTH, HEIGHT);
 
         const random = rnd(12345);
         const random2 = rnd(1234);
@@ -173,13 +196,14 @@
             const y = random() * (HEIGHT - 10) + 10;
             const boundary = intercept + x * slope + randomNormal(random2) * 30;
             const color = y >= boundary ? COL_ORANGE : COL_GREEN;
-            addSample(parent, MARGIN_X + x, HEIGHT - y, color);
+            addSample(parent, marginX + x, HEIGHT - y, color);
         }
 
         const predictLine = addPredictionLine(
             parent,
             [0, intercept],
-            [0.3, slope]
+            [0.3, slope],
+            marginX
         );
 
         $fragmentOrSlide.on('show', () => {
@@ -200,7 +224,7 @@
         $fragmentOrSlide.on('show', () => {
             setTimeout(() => {
                 $('#dt-q1').fadeIn();
-                
+
                 setTimeout(() => {
                     $('#dt-r1').fadeIn();
                     $('#dt-q2').fadeIn();
@@ -360,6 +384,158 @@
                 clearTimeout(nextAnimTimeout);
                 nextAnimTimeout = null;
             }
+        });
+    }
+
+    function bindGradientDescent(fragmentOrSlideId, elementId) {
+        const $fragmentOrSlide = $('#' + fragmentOrSlideId);
+        const parent = document.getElementById(elementId);
+
+        const MARGIN_X = 450;
+
+        addGridLine(parent, MARGIN_X, 0, MARGIN_X, HEIGHT);
+        addGridLine(parent, MARGIN_X, HEIGHT, MARGIN_X + WIDTH, HEIGHT);
+
+        const random = rnd(12345);
+
+        const realIntercept = 0.13333;
+        const realSlope = 0.4;
+
+        const points = [];
+        for (let x = 0.05; x < (WIDTH / HEIGHT); x += random() * 0.05) {
+            const y = realIntercept + x * realSlope + randomNormal(random) * (0.015 + x * 0.1);
+            points.push({x, y});
+            addSample(parent, MARGIN_X + x * HEIGHT, (1 - y) * HEIGHT, COL_ORANGE);
+        }
+
+        const learningRate = 0.1;
+        let params = {intercept: 0.4, slope: -0.6};
+
+        const gradient = (params) => {
+            let iGrad = 0.0;
+            let sGrad = 0.0;
+            const n = points.length;
+            points.forEach(point => {
+                const yHat = (params.slope * point.x) + params.intercept;
+                const error = point.y - yHat;
+                iGrad += -(2 / n) * error;
+                sGrad += -(2 / n) * point.x * error;
+            });
+            return [iGrad, sGrad];
+        };
+
+        const gradientStep = () => {
+            const grad = gradient(params);
+            return {
+                intercept: params.intercept - (learningRate * grad[0]),
+                slope: params.slope - (learningRate * grad[1])
+            };
+        };
+
+        const SAMPLES_DIM = 300;
+
+        const I_MIN = -0.4;
+        const I_MAX = 1.2;
+        const S_MIN = -0.7;
+        const S_MAX = 0.7;
+
+        const sampleError = () => {
+            const errors = [];
+            
+            for (let y = SAMPLES_DIM - 1; y >= 0; y--) {
+                const slope = S_MIN + (S_MAX - S_MIN) * (y / SAMPLES_DIM);
+                for (let x = 0; x < SAMPLES_DIM; x ++) {
+                    const intercept = I_MIN + (I_MAX - I_MIN) * (x / SAMPLES_DIM);
+                    let err = 0;
+                    points.forEach(point => {
+                        const yHat = (slope * point.x) + intercept;
+                        err += Math.pow(point.y - yHat, 2);
+                    });
+                    
+
+                    errors.push(Math.sqrt(err));
+                }
+            }
+            return errors;
+        };
+
+        const $errPlot = $('<canvas id="error-plot" width="300" height="300">');
+        const errPlot = $errPlot[0];
+        const ctx = errPlot.getContext("2d");
+        const imgData = ctx.getImageData(0, 0, SAMPLES_DIM, SAMPLES_DIM);
+        const data = imgData.data;
+
+        const errValues = sampleError();
+        let maxErr = 0;
+        let minErr = Infinity;
+        errValues.forEach(err => {
+            if (err > maxErr) {
+                maxErr = err;
+            }
+            if (err < minErr) {
+                minErr = err;
+            }
+        });
+
+        const colorScale = chroma.scale([
+            "#381e3c",
+            '#2b55c0',
+            '#178a42',
+            '#b6ad23',
+            '#d45919',
+            '#d40800',
+        ]).mode('lab');
+        
+        const colorCache = {};
+        
+        for (let i = 0; i < errValues.length; i++) {
+            const err = errValues[i] / maxErr;
+            const idx = i * 4;
+            const colorCacheKey = Math.round(err * 25) / 25;
+            if (colorCache[colorCacheKey] === undefined) {
+                colorCache[colorCacheKey] = colorScale(colorCacheKey);
+            }
+            const col = colorCache[colorCacheKey];
+            data[idx] = col.get('rgb.r');
+            data[idx + 1] = col.get('rgb.g');
+            data[idx + 2] = col.get('rgb.b');
+            data[idx + 3] = 255
+        }
+        ctx.putImageData(imgData, 0, 0);
+
+        $(parent).append($errPlot);
+
+        const recordSteps = [0, 1, 2, 4, 8, 16, 30, 50,75, 512];
+        let predictLine = addPredictionLine(parent, [params.intercept * HEIGHT], [params.slope], MARGIN_X);
+        let stepLines = [];
+
+        let prevParams = params;
+        for (let i = 0; i <= 512; i++) {
+            params = gradientStep();
+            
+            if (recordSteps.indexOf(i) > -1) {
+                predictLine = updatePredictLine(predictLine, params.intercept * HEIGHT, params.slope, 250, MARGIN_X);
+                predictLine = updatePredictLine(predictLine, params.intercept * HEIGHT, params.slope, 500, MARGIN_X);
+                
+                const x1 = ((prevParams.intercept - I_MIN) / (I_MAX - I_MIN)) * SAMPLES_DIM;
+                const y1 = ((prevParams.slope - S_MIN) / (S_MAX - S_MIN)) * SAMPLES_DIM;
+                const x2 = ((params.intercept - I_MIN) / (I_MAX - I_MIN)) * SAMPLES_DIM;
+                const y2 = ((params.slope - S_MIN) / (S_MAX - S_MIN)) * SAMPLES_DIM;
+                
+                stepLines.push(addStepLine(parent, x1, y1, x2, y2, 60, 1000 + recordSteps.indexOf(i) * 750));
+                prevParams = params;
+            }
+        }
+
+        $fragmentOrSlide.on('show', () => {
+            predictLine.play();
+            stepLines.forEach(l => l.play());
+        });
+
+        $fragmentOrSlide.on('hide', () => {
+            predictLine.reset();
+            stepLines.forEach(l => l.stop());
+            stepLines.forEach(l => l.reset());
         });
     }
 })();
